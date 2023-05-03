60e92fb88542d5d425c14578d85025ef55d36753
Anton Nevero <nevero.anton@gmail.com>
2023-05-03 18:13:59 +0300

    Add reservation

--- Check_ec2_health/main.py
+++ Check_ec2_health/main.py
@@ -3,5 +3,7 @@ import boto3
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
 
-instances = ec2_client.describe_instances()
+reservations = ec2_client.describe_instances()
+for reservation in reservations['Reservations']:
+
 print(instances)

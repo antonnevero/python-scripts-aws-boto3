6f5224cd282c6363f6e6a16ccd856ff7c8c118e5
Anton Nevero <nevero.anton@gmail.com>
2023-05-03 17:23:25 +0300

    Add subnets

--- Project1/main.py
+++ Project1/main.py
@@ -6,7 +6,13 @@ ec2_resource = boto3.resource('ec2')
 new_vpc = ec2_resource.create_vpc(
     CidrBlock="10.0.0.0/16"
 )
-new_vpc.create_subnet()
+new_vpc.create_subnet(
+    CidrBlock="10.0.1.0/24"
+)
+
+new_vpc.create_subnet(
+    CidrBlock="10.0.2.0/24"
+)
 
 all_available_vpcs = ec2_client.describe_vpcs()
 vpcs = all_available_vpcs["Vpcs"]

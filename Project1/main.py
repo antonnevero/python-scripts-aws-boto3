67e59480da342351c3546f19dd3446fc39a583bd
Anton Nevero <nevero.anton@gmail.com>
2023-05-03 17:21:18 +0300

    Add ec2 resource

--- Project1/main.py
+++ Project1/main.py
@@ -1,6 +1,12 @@
 import boto3
 
 ec2_client = boto3.client('ec2')
+ec2_resource = boto3.resource('ec2')
+
+new_vpc = ec2_resource.create_vpc(
+    CidrBlock="10.0.0.0/16"
+)
+new_vpc.create_subnet()
 
 all_available_vpcs = ec2_client.describe_vpcs()
 vpcs = all_available_vpcs["Vpcs"]

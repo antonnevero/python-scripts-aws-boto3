877e5e21e806a591ba588c882dc9b71953b22491
Anton Nevero <nevero.anton@gmail.com>
2023-05-04 17:45:37 +0300

    Check system status

--- Check_ec2_health/main.py
+++ Check_ec2_health/main.py
@@ -8,3 +8,9 @@ for reservation in reservations['Reservations']:
     instances = reservation['Instances']
     for instance in instances:
         print(f"Status of instance {instance['InstanceId']} is {instance['State']['Name']}")
+
+statuses = ec2_client.describe_instance_status()
+for status in statuses['InstanceStatuses']:
+    ins_status = status['InstanceStatus']['Status']
+    sys_status = status['SystemStatus']['Status']
+    print(f"Instance {status['InstanceId']} status is {ins_status} and system status is {sys_status}")

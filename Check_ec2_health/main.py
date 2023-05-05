3154f36079294e06c2cf2a3e7c34bdffad638fdd
Anton Nevero <nevero.anton@gmail.com>
2023-05-05 19:24:55 +0300

    Add schedule

--- Check_ec2_health/main.py
+++ Check_ec2_health/main.py
@@ -1,17 +1,28 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
 
-reservations = ec2_client.describe_instances()
-for reservation in reservations['Reservations']:
-    instances = reservation['Instances']
-    for instance in instances:
-        print(f"Ip address of instance {instance['InstanceId']} is {instance['PublicIpAddress']}")
 
-statuses = ec2_client.describe_instance_status()
-for status in statuses['InstanceStatuses']:
-    ins_status = status['InstanceStatus']['Status']
-    sys_status = status['SystemStatus']['Status']
-    state = status["InstanceState"]['Name']
-    print(f"Instance {status['InstanceId']} is {state} with instance status is {ins_status} and system status is {sys_status}")
+def check_instance_ip():
+    reservations = ec2_client.describe_instances()
+    for reservation in reservations['Reservations']:
+        instances = reservation['Instances']
+        for instance in instances:
+            print(f"Ip address of instance {instance['InstanceId']} is {instance['PublicIpAddress']}")
+
+
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status()
+    for status in statuses['InstanceStatuses']:
+        ins_status = status['InstanceStatus']['Status']
+        sys_status = status['SystemStatus']['Status']
+        state = status["InstanceState"]['Name']
+        print(f"Instance {status['InstanceId']} is {state} with instance status is {ins_status} and system status is {sys_status}")
+
+
+schedule.every(5).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()

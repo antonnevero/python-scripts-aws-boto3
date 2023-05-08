3fe8fd7a4976c306fd47b86c65ede87a0c0524ea
Anton Nevero <nevero.anton@gmail.com>
2023-05-08 18:45:49 +0300

    Add script for volumes backups

---
+++ Backups_volumes_ec2/backups.py
@@ -0,0 +1,19 @@
+import boto3
+import schedule
+
+ec_client = boto3.client('ec2')
+
+
+def create_volume_snapshots():
+    volumes = ec_client.describe_volumes()
+    for volume in volumes['Volumes']:
+        new_snapshot = ec_client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+        print(new_snapshot)
+
+
+schedule.every().day.do(create_volume_snapshots)
+
+while True:
+    schedule.run_pending()

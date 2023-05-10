e2aa46b18922b944a73630c151da9da2c5764aec
Anton Nevero <nevero.anton@gmail.com>
2023-05-10 19:05:03 +0300

    Add monitoring site project 2

--- Website_monitoring/website_monitoring.py
+++ Website_monitoring/website_monitoring.py
@@ -1,4 +1,7 @@
 import requests
 
 response = requests.get('http://35.158.218.87/')
-print(response.status_code)
+if response.status_code == 200:
+    print("Application is running successfully")
+else:
+    print("Application Down, Fix it!")

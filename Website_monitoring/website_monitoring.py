b5c9fbf7019c6e8311fc20007dbcb2390c18f5a5
Anton Nevero <nevero.anton@gmail.com>
2023-05-11 17:34:09 +0300

    Add smtplib

--- Website_monitoring/website_monitoring.py
+++ Website_monitoring/website_monitoring.py
@@ -1,7 +1,17 @@
 import requests
+import smtplib
+import os
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
 
 response = requests.get('http://35.158.218.87/')
 if response.status_code == 200:
     print("Application is running successfully")
 else:
     print("Application Down, Fix it!")
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
